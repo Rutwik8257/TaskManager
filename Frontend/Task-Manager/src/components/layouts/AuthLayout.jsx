468b2200870ed4302cec9d7c13021028e20dda89
Rutwik8257 <139038132+Rutwik8257@users.noreply.github.com>
2025-07-12 14:06:21 +0530

    Update AuthLayout.jsx

--- Frontend/Task-Manager/src/components/layouts/AuthLayout.jsx
+++ Frontend/Task-Manager/src/components/layouts/AuthLayout.jsx
@@ -10,7 +10,10 @@ const AuthLayout = ({ children }) => {
       </div>
 
       {/* Right Side Image Panel */}
-      <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-[url('/bg-img.jpg')] bg-cover bg-no-repeat bg-center overflow-hidden p-8">
+      <div
+        className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-cover bg-no-repeat bg-center overflow-hidden p-8"
+        style={{ backgroundImage: "url('/bg-img.jpg')" }}
+      >
         <img src={UI_img} className="w-64 lg:w-[90%]" alt="Auth" />
       </div>
     </div>
